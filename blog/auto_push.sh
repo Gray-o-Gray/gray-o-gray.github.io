494d18e9ccc23d10178d5f9bc014b0e859970285
miki <zhu970427@qq.com>
2024-12-01 20:14:09 +0800

    Sun Dec  1 20:14:09     2024 UPGRADE: auto

--- blog/auto_push.sh
+++ blog/auto_push.sh
@@ -22,31 +22,31 @@ hexo_develop_to_git(){
     echo "hexo develop to github.io finished."
 }
 
-while getopts ":g:d:h" optname
+while getopts ":g:dh" optname
 do
     case "$optname" in
-      "g")
-        echo "get option -g,value is $OPTARG"
-        git_commit_push $OPTARG
-        ;;
-      "d")
-        echo "get option -d"
-        hexo_develop_to_git
-        ;;
-      "h")
-        echo "get option -h"
-        echo "-g: auto git commit and push. it'll use argument as commit content."
-        echo "-d: auto hexo develop to github.io finished."
-        ;;
-      ":")
-        echo "No argument value for option $OPTARG"
-        ;;
-      "?")
-        echo "Unknown option $OPTARG"
-        ;;
-      *)
-        echo "Unknown error while processing options"
-        ;;
+        "g")
+            echo "get option -g,value is $OPTARG"
+            git_commit_push $OPTARG
+            ;;  
+        "d")
+            echo "get option -d"
+            hexo_develop_to_git
+            ;;
+        "h")
+            echo "get option -h"
+            echo "-g: auto git commit and push. it'll use argument as commit content."
+            echo "-d: auto hexo develop to github.io finished."
+            ;;
+        ":")
+            echo "No argument value for option $OPTARG"
+            ;;
+        "?")
+            echo "Unknown option $OPTARG"
+            ;;
+        *)
+            echo "Unknown error while processing options"
+            ;;
     esac
     #echo "option index is $OPTIND"
 done
