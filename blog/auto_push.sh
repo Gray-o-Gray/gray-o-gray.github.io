26a7211ff8f7f61f4587c40b7415f6227404e844
miki <zhu970427@qq.com>
2024-12-01 20:08:27 +0800

    Sun Dec  1 20:08:27     2024 UPGRADE:

--- blog/auto_push.sh
+++ blog/auto_push.sh
@@ -1,14 +1,54 @@
 #!/bin/bash
 
-git add .
+git_commit_push(){
+    
+    git add .
 
-time=$(date)
+    local time=$(date)
 
-commit=$1
+    local commit=$1
 
-git commit -m "${time} UPGRADE: ${commit}"
+    git commit -m "${time} UPGRADE: ${commit}"
 
-git push origin hexo
+    git push origin hexo
 
-hexo d -g || npx hexo d -g
+    echo "git commit and push finished."
+}
+
+hexo_develop_to_git(){
+
+    hexo d -g || npx hexo d -g
+
+    echo "hexo develop to github.io finished."
+}
+
+while getopts ":g:d:h" optname
+do
+    case "$optname" in
+      "g")
+        echo "get option -g,value is $OPTARG"
+        git_commit_push $OPTARG
+        ;;
+      "d")
+        echo "get option -d"
+        hexo_develop_to_git
+        ;;
+      "h")
+        echo "get option -h"
+        echo "-g: auto git commit and push. it'll use argument as commit content."
+        echo "-d: auto hexo develop to github.io finished."
+        ;;
+    #   ":")
+    #     echo "No argument value for option $OPTARG"
+    #     ;;
+      "?")
+        echo "Unknown option $OPTARG"
+        ;;
+      *)
+        echo "Unknown error while processing options"
+        ;;
+    esac
+    #echo "option index is $OPTIND"
+done
+
 
